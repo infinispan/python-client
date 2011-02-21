e3aabb6c41f9267e29b8563884872d47900cdc7e
Galder Zamarreño <galder@jboss.org>
2011-02-21 15:39:00 +0100

    Adding long description for PyPI

--- setup.py
+++ setup.py
@@ -12,17 +12,18 @@ setup(name = 'infinispan',
       author_email = 'galder@jboss.org',
       url = 'http://infinispan.org',
       py_modules = ['infinispan.remotecache', 'infinispan.unsigned'],
-      classifiers=[
+      classifiers = [
           "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
           "Programming Language :: Python",
           "Development Status :: 4 - Beta",
           "Intended Audience :: Developers",
           "Topic :: Software Development :: Libraries :: Python Module",
       ],
-      keywords='infinispan hotrod nosql datagrid',
-      license='LGPL',
-      install_requires=[
+      keywords = 'infinispan hotrod nosql datagrid',
+      license = 'LGPL',
+      install_requires = [
         'setuptools',
         'greenlet',
       ],
+      long_description = "See `Infinispan Native Python Client <https://github.com/infinispan/python-client>`_ for more information."
       )
